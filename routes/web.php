60f35b6f0ca5c29ac7e648f80b67d9945021d95e
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-06-30 17:20:30 +0900

    feat: お問い合わせ詳細ページの実装 #14

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts', [ContactController::class, 'store'])->name('contact.store');
 Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
 Route::get('/thanks', [ContactController::class, 'thanks'])->name('contact.thanks');
+Route::get('admin/contacts/{contact}', [AdminController::class, 'show'])->name('admin.contacts.show');
+Route::delete('admin/contacts/{contact}', [AdminController::class, 'destroy'])->name('admin.contacts.destroy');
 Route::post('/admin/tags', [TagController::class, 'store'])->name('admin.tags.store');
 Route::get('/admin/tags/{tag}/edit', [TagController::class, 'edit'])->name('admin.tags.edit');
 Route::put('/admin/tags/{tag}', [TagController::class, 'update'])->name('admin.tags.update');
