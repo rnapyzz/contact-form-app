3fdb0e476ea420dc9e29c40a287554355d34a2ec
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-06-30 16:55:49 +0900

    feat: タグの追加・編集・削除機能の実装 #5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,7 @@ Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts', [ContactController::class, 'store'])->name('contact.store');
 Route::post('/contacts/confirm', [ContactController::class, 'confirm'])->name('contact.confirm');
 Route::get('/thanks', [ContactController::class, 'thanks'])->name('contact.thanks');
+Route::post('/admin/tags', [TagController::class, 'store'])->name('admin.tags.store');
+Route::get('/admin/tags/{tag}/edit', [TagController::class, 'edit'])->name('admin.tags.edit');
+Route::put('/admin/tags/{tag}', [TagController::class, 'update'])->name('admin.tags.update');
+Route::delete('/admin/tags/{tag}', [TagController::class, 'destroy'])->name('admin.tags.destroy');
